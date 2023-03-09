9b4bc052b06b1e998114e8c2de95d64f940354c1
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2023-03-09 10:58:21 -0300

    d

--- API/Program.cs
+++ API/Program.cs
@@ -10,28 +10,29 @@ using SendGrid.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<LuxonDB>(opts=>
+// builder.Services.AddDbContext<LuxonDB>(opts=>
+// {
+//     opts.UseNpgsql (builder.Configuration.GetConnectionString("LC"));
+// });
+
+builder.Services.AddDbContext<LuxonDB>(options =>
 {
-    opts.UseNpgsql (builder.Configuration.GetConnectionString("LC"));
+       // Use connection string provided at runtime by FlyIO.
+       var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+       // Parse connection URL to connection string for Npgsql
+       connUrl = connUrl.Replace("postgres://", string.Empty);
+       var pgUserPass = connUrl.Split("@")[0];
+       var pgHostPortDb = connUrl.Split("@")[1];
+       var pgHostPort = pgHostPortDb.Split("/")[0];
+       var pgDb = pgHostPortDb.Split("/")[1];
+       var pgUser = pgUserPass.Split(":")[0];
+       var pgPass = pgUserPass.Split(":")[1];
+       var pgHost = pgHostPort.Split(":")[0];
+       var pgPort = pgHostPort.Split(":")[1];
+       var connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+       options.UseNpgsql(connStr);      
 });
 
-// builder.Services.AddDbContext<LuxonDB>(options =>
-// {
-//        // Use connection string provided at runtime by FlyIO.
-//        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-//        // Parse connection URL to connection string for Npgsql
-//        connUrl = connUrl.Replace("postgres://", string.Empty);
-//        var pgUserPass = connUrl.Split("@")[0];
-//        var pgHostPortDb = connUrl.Split("@")[1];
-//        var pgHostPort = pgHostPortDb.Split("/")[0];
-//        var pgDb = pgHostPortDb.Split("/")[1];
-//        var pgUser = pgUserPass.Split(":")[0];
-//        var pgPass = pgUserPass.Split(":")[1];
-//        var pgHost = pgHostPort.Split(":")[0];
-//        var pgPort = pgHostPort.Split(":")[1];
-//        var connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-//        options.UseNpgsql(connStr);      
-// });
 builder.Services.AddSendGrid(options =>
 {
     // options.ApiKey = (builder.Configuration.GetSection("SendGridApiKey").Value);
